03d69716b69f1cd3f9b8e47a864bf78c23d5a6c7
Ythosa <vasus714@yandex.ru>
2021-08-18 19:08:34 +0300

    base of apigateway

--- ApiGateway/build.gradle.kts
+++ ApiGateway/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("plugin.spring") version "1.5.21"
 }
 
-group = "com.ythosa.photoapp.discovery"
+group = "com.ythosa.photoapp.api"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
@@ -18,12 +18,16 @@ repositories {
 extra["springCloudVersion"] = "2020.0.3"
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
-    implementation("com.sun.jersey.contribs:jersey-apache-client4")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 dependencyManagement {
