e549212c7cd2473286ff0babf374eaa8fef391a0
Ythosa <vasus714@yandex.ru>
2021-08-18 14:52:05 +0300

    base of eureka service

---
+++ PhotoAppDiscoveryService/src/test/kotlin/com/ythosa/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ythosa.photoapp.discovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
